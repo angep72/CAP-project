9adda50352bdffcdb798794c022c90be76e4c0d2
Pauline Umunyana <pauline.umunyana@moyotech.solutions>
2024-12-24 16:29:33 +0200

    connecting backend to frontui

---
+++ com.myorg.myapp/webapp/controller/Main.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+   "./BaseController",
+   "sap/m/MessageBox",
+   "sap/ui/model/odata/v4/ODataModel"
+], function (BaseController, MessageBox, ODataModel) {
+   "use strict";
+
+   return BaseController.extend("com.myorg.myapp.controller.Main", {
+    onInit: function () {
+        const oModel = new ODataModel({
+            serviceUrl: "http://localhost:4000/odata/",
+            synchronizationMode: "None"
+        });
+        
+        this.getView().setModel(oModel);
+    }
+   });
+});
